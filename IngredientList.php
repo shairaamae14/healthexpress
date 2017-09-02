bfc4d8438bdb93688a8b69abb054a08865215288
Josephine Jabagat <30436769+jojabagat@users.noreply.github.com>
2017-09-02 22:59:14 +0800

    Add files via upload

---
+++ IngredientList.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class IngredientList extends Model
+{
+    protected $table = 'ingredient_list';
+    protected $primaryKey = 'id';
+}
