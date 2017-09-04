06e1823368b605d31a64ce96b19bbc152f815840
Josephine Jabagat <30436769+jojabagat@users.noreply.github.com>
2017-09-04 15:59:52 +0800

    Add files via upload

---
+++ app/UnitMeasurement.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UnitMeasurement extends Model
+{
+    protected $table = 'unit_measurements';
+    protected $primaryKey = 'um_id';
+}
