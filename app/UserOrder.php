7058ea5bc3ee36c52a5c413f1fac1297804c071c
beatrice.ylaya <ylayabeatrice@gmail.com>
2017-10-07 15:01:19 +0800

    - user order

---
+++ app/UserOrder.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserOrder extends Model
+{
+    protected $fillable = ['user_id', 'order_id', 'payment_id', 'dish_id',
+                            'order_date', 'totalQty', 'totalAmount', 'order_status'];
+}
