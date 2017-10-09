ebc6342f24f79fd2e1bb4676ac72f548f810cd57
beatrice.ylaya <ylayabeatrice@gmail.com>
2017-10-10 00:35:42 +0800

    changes: -nav

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -22,38 +22,58 @@
                 </div>
             </a>
         </div>
-<div class="collapse navbar-collapse" id="example-navbar-success">
+        <div class="collapse navbar-collapse" id="example-navbar-success">
             <ul class="nav navbar-nav navbar-right">
-               
+
                 <li>
                     <a href="http://demos.creative-tim.com/material-kit-pro/presentation.html?ref=utp-freebie" target="_blank">
-                            <i class="material-icons">help</i> Help
+                        <i class="material-icons">help</i> Help
                     </a>
                 </li>
-                    <li>
-                        <a href="http://demos.creative-tim.com/material-kit-pro/presentation.html?ref=utp-freebie" target="_blank">
-                            How it works
-                        </a>
-                    </li>
+                <li>
+                    <a href="#works">
+                        How it works
+                    </a>
+                </li>
 
-                    <li>
-                        <a href="http://demos.creative-tim.com/material-kit-pro/presentation.html?ref=utp-freebie" target="_blank">
-                         About us
-                        </a>
-                    </li>
+                <li>
+                    <a href="#about">
+                        About us
+                    </a>
+                </li>
+                @if(Auth::guest())
+                <li>
+                    <a href="./login">
+                        Login
+                    </a>
+                </li>
 
-                    <li>
-                        <a href="./login" target="_blank">
-                          Login
-                        </a>
-                    </li>
+                <li>
+                    <a href="./register" target="_blank">
+                        Register
+                    </a>
+                </li>
+                @else
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        <i class="material-icons">account_circle</i> {{ Auth::user()->fname}} {{Auth::user()->lname}}</a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="{{route('user.profile', ['id'=> Auth::id()])}}">Profile</a></li>
+                        <li><a href="#">Order History</a></li>
+                        <li>
+                            <a href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                       document.getElementById('logout-form').submit();">
+                                Logout
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+                @endif
 
-                    <li>
-                        <a href="./register" target="_blank">
-                          Register
-                        </a>
-                    </li>
-                   
 
             </ul>
         </div>
