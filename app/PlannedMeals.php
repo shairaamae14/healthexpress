cecaa2319be26cdd4aaa459c33facdae9a0f26d5
Josephine Jabagat <30436769+jojabagat@users.noreply.github.com>
2018-02-04 22:36:50 +0800

    Planned meals
    planned meals model

--- app/PlannedMeals.php
+++ app/PlannedMeals.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class PlannedMeals extends Model
 {
     protected $table = 'planned_meals';
-    protected $primaryKey = 'id';
-    protected $fillable = ['title','user_id','om_id','dish_id','be_id','plan_id','start','end','allDay'];
+    protected $primaryKey = 'pm_id';
+    protected $fillable = ['title','user_id','om_id','dish_id','be_id','plan_id','p_status','start','end','allDay','note','order_status','mode_delivery', 'address'];
 
-
+    
 }
