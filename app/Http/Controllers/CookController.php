2e5bead9dc75717a4d74aaf706182fc135ad9c1f
Josephine Jabagat <30436769+jojabagat@users.noreply.github.com>
2018-02-04 20:00:33 +0800

    Controller
    -cook

--- app/Http/Controllers/CookController.php
+++ app/Http/Controllers/CookController.php
@@ -122,11 +122,10 @@ class CookController extends Controller
                             ->join('users', 'planned_meals.user_id', '=', 'users.id')
                             ->join('dish_besteaten','dishes.did', '=', 'dish_besteaten.dish_id')
                             ->join('besteaten_at', 'dish_besteaten.be_id' , '=', 'besteaten_at.be_id')
-                            ->join('plans', 'planned_meals.plan_id', '=', 'plans.id')
                             ->join('order_mode', 'planned_meals.om_id', '=', 'order_mode.id')
                             ->where('order_status', 'ordered')
                             ->distinct()
-                            ->get(['fname','plan_id','plan_name','lname','om_name','user_id','note']);
+                            ->get(['fname','lname','om_name','user_id','note']);
                             // dd($fetch);
 
 
